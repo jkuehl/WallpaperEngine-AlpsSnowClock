220d6ff21644409ba7d448aa174552bc6d39179d
johnr <rayben@gmail.com>
2021-04-03 18:59:36 +0200

    Initial commit.

---
+++ js/time.js
@@ -0,0 +1,32 @@
+function showLocale(objD)  
+{  
+    var str,colorhead,colorfoot;  
+    var hh = objD.getHours();  
+    if (hh < 12) var morning = true;
+    if (hh < 10) hh = '0' + hh;
+
+    var mm = objD.getMinutes();  
+    if(mm<10) mm = '0' + mm;  
+
+    var ww = objD.getDay();  
+    if  ( ww==0 )  colorhead="<font color=\"white\">";  
+    if  ( ww > 0 && ww < 7 )  colorhead="<font color=\"white\">";  
+    if  (ww==1)  ww="Montag";  
+    if  (ww==2)  ww="Dienstag";  
+    if  (ww==3)  ww="Mittwoch";  
+    if  (ww==4)  ww="Donnerstag";  
+    if  (ww==5)  ww="Freitag";  
+    if  (ww==6)  ww="Samstag";
+    if  (ww==0)  ww="Sonntag";  
+        colorfoot="</font>"  
+    str = colorhead + "<span class=\"thin\">" + hh + ":"  + mm + "</span>"+ "<br>" + ww  + "  " + colorfoot;  
+    return(str);  
+};  
+function tick()  
+{  
+    var today;  
+    today = new Date();  
+    document.getElementById("localtime").innerHTML = showLocale(today);  
+    window.setTimeout("tick()", 1000);  
+};  
+tick();  
